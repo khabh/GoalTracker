08604b9f0ac6216105bca47756bf4a2a7ec42dbf
khabh <khabh@naver.com>
2023-08-23 15:20:15 +0900

    feat(Checklist/feat-goal): 자주 완료된 Checklist 항목 조회 기능 구현#1

--- goaltracker/src/main/java/com/goaltracker/checklist/service/ChecklistService.java
+++ goaltracker/src/main/java/com/goaltracker/checklist/service/ChecklistService.java
@@ -1,9 +1,13 @@
 package com.goaltracker.checklist.service;
 
 import com.goaltracker.checklist.dto.CreateChecklistsDTO;
+import com.goaltracker.checklist.dto.PopularCompletedChecklistDTO;
 import com.goaltracker.goal.domain.Goal;
+
+import java.util.List;
 
 public interface ChecklistService {
 
-    public void createChecklists(CreateChecklistsDTO createChecklistsDTO, Goal goal);
+    void createChecklists(CreateChecklistsDTO createChecklistsDTO, Goal goal);
+    List<PopularCompletedChecklistDTO> getPopularCompletedChecklists(Goal goal);
 }
--- goaltracker/src/main/java/com/goaltracker/checklist/service/ChecklistServiceImpl.java
+++ goaltracker/src/main/java/com/goaltracker/checklist/service/ChecklistServiceImpl.java
@@ -2,6 +2,7 @@ package com.goaltracker.checklist.service;
 
 import com.goaltracker.checklist.domain.Checklist;
 import com.goaltracker.checklist.dto.CreateChecklistsDTO;
+import com.goaltracker.checklist.dto.PopularCompletedChecklistDTO;
 import com.goaltracker.checklist.repository.ChecklistRepository;
 import com.goaltracker.checklist.util.ChecklistConverter;
 import com.goaltracker.goal.domain.Goal;
@@ -24,5 +25,10 @@ public class ChecklistServiceImpl implements ChecklistService {
     public void createChecklists(CreateChecklistsDTO createChecklistsDTO, Goal goal) {
         List<Checklist> checklists = ChecklistConverter.toChecklists(createChecklistsDTO, goal);
         checklistRepository.saveAll(checklists);
+    }
+
+    @Override
+    public List<PopularCompletedChecklistDTO> getPopularCompletedChecklists(Goal goal) {
+        return checklistRepository.findPopularCompletedChecklists(goal);
     }
 }
