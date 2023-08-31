ac262cfa309cb084e69d908429bf2b4332558155
khabh <khabh@naver.com>
2023-08-31 15:03:29 +0900

    feat(User): 회원가입 입력값 검증 DTO 생성 #12

---
+++ goaltracker/src/main/java/com/goaltracker/user/dto/CreateUserDTO.java
@@ -0,0 +1,26 @@
+package com.goaltracker.user.dto;
+
+import com.goaltracker.user.validator.annotation.NoWhitespace;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateUserDTO {
+    @NoWhitespace
+    @Size(min = 3, max = 11, message = "3에서 11 글자 사이로 입력해 주세요.")
+    private String username;
+
+    @NotBlank(message = "이메일에는 공백이 포함될 수 없습니다.")
+    @Email(message = "형식이 올바르지 않은 이메일입니다.")
+    private String email;
+
+    @NoWhitespace
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+            message = "대소문자, 숫자, 특수문자를 포함한 8에서 20자리 문자열로 입력해 주세요.")
+    private String password;
+}
